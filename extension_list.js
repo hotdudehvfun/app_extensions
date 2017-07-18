5fb7dc1910021f6a0d698207ae71d32edd3d2963
hotdudehvfun <hotdudehvfun@gmail.com>
2017-07-18 14:24:35 +0530

    Create extension_list.js

---
+++ extension_list.js
@@ -0,0 +1,20 @@
+{
+	"extensions":
+	[
+		{
+	"icon":"R.string.school",
+	"icon_color":[255,233,30,99],
+	"title":"Synonyms",
+	"subtitle":"Learn similar meaning words",
+	"url":"https://hotdudehvfun.github.io/app_extensions/extension_synonyms.html"
+},
+		{
+	"icon":"R.string.school",
+	"icon_color":[255,233,30,99],
+	"title":"Idioms and Phrases",
+	"subtitle":"Learn idioms and phrasal verbs",
+	"url":"https://hotdudehvfun.github.io/app_extensions/extension_maxims.html"
+}
+	]
+}
+
