170e4b82a92947a468573c69a7467b169fb271b2
hotdudehvfun <hotdudehvfun@gmail.com>
2017-07-18 14:51:02 +0530

    Update extension_list.js

--- extension_list.js
+++ extension_list.js
@@ -3,14 +3,14 @@
 	[
 		{
 	"icon":"R.string.school",
-	"icon_color":[255,233,30,99],
+	"icon_color":"#e91e63",
 	"title":"Synonyms",
 	"subtitle":"Learn similar meaning words",
 	"url":"https://hotdudehvfun.github.io/app_extensions/extension_synonyms.html"
 },
 		{
 	"icon":"R.string.school",
-	"icon_color":[255,233,30,99],
+	"icon_color":"#e91e63",
 	"title":"Idioms and Phrases",
 	"subtitle":"Learn idioms and phrasal verbs",
 	"url":"https://hotdudehvfun.github.io/app_extensions/extension_maxims.html"
