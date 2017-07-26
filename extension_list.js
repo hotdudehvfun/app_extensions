4279dd9a6494dc84d930fd9e383f0c7c2721330c
hotdudehvfun <hotdudehvfun@gmail.com>
2017-07-26 16:24:17 +0530

    Update extension_list.js

--- extension_list.js
+++ extension_list.js
@@ -3,6 +3,20 @@
     {
       "icon": "school",
       "icon_color": "#e91e63",
+      "title": "Verbal Series",
+      "subtitle": "Learn opposite meaning words",
+      "url": "https://hotdudehvfun.github.io/app_extensions/extension_verbal_series.html"
+    },
+    {
+      "icon": "school",
+      "icon_color": "#e91e63",
+      "title": "Missing Sequence",
+      "subtitle": "Learn opposite meaning words",
+      "url": "https://hotdudehvfun.github.io/app_extensions/extension_gap_sequence.html"
+    },
+    {
+      "icon": "school",
+      "icon_color": "#e91e63",
       "title": "Antonyms",
       "subtitle": "Learn opposite meaning words",
       "url": "https://hotdudehvfun.github.io/app_extensions/extension_antonyms.html"
