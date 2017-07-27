5dfeb777427d00d3566c19e5968b3abd69389f45
hotdudehvfun <hotdudehvfun@gmail.com>
2017-07-27 19:00:35 +0530

    Update extension_list.js

--- extension_list.js
+++ extension_list.js
@@ -1,35 +1,35 @@
 {
   "extensions": [
     {
-      "icon": "school",
+      "icon": "group_work",
       "icon_color": "#4CD964",
       "title": "Missing Number",
       "subtitle": "Find missing number in a matrix",
       "url": "https://hotdudehvfun.github.io/app_extensions/extension_missing_number.html"
     }
     ,{
-      "icon": "school",
-      "icon_color": "#e91e63",
+      "icon": "line_style",
+      "icon_color": "#5AC8FA",
       "title": "Verbal Series",
       "subtitle": "Find next series in a word/letter series",
       "url": "https://hotdudehvfun.github.io/app_extensions/extension_verbal_series.html"
     },
     {
-      "icon": "school",
-      "icon_color": "#e91e63",
+      "icon": "view_module",
+      "icon_color": "#5AC8FA",
       "title": "Missing Sequence",
       "subtitle": "A sequence is given, you have to find missing letters.",
       "url": "https://hotdudehvfun.github.io/app_extensions/extension_gap_sequence.html"
     },
     {
-      "icon": "school",
+      "icon": "library_books",
       "icon_color": "#e91e63",
       "title": "Antonyms",
       "subtitle": "Learn opposite meaning words",
       "url": "https://hotdudehvfun.github.io/app_extensions/extension_antonyms.html"
     },
     {
-      "icon": "school",
+      "icon": "library_books",
       "icon_color": "#e91e63",
       "title": "Synonyms",
       "subtitle": "Learn similar meaning words",
@@ -40,7 +40,7 @@
       "icon_color": "#FFC107",
       "title": "Idioms and Phrases",
       "subtitle": "Learn idioms and phrasal verbs",
-      "url": "https://hotdudehvfun.github.io/app_extensions/extension_maxims.html"
+      "url": "https://hotdudehvfun.github.io/app_extensions/extension_idioms.html"
     }
   ]
 }
