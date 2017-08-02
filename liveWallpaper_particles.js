b09265fa9ee554f7f0a2edf42ec89e119fa47108
hotdudehvfun <hotdudehvfun@gmail.com>
2017-08-02 12:19:27 +0530

    Add files via upload

--- liveWallpaper_particles.js
+++ liveWallpaper_particles.js
@@ -1,7 +1,17 @@
 var canvas,context;
-var lettersList=[];
-var questionItem={};
+var particleList=[];
 var textDir=1;
+var materialColors=[];
+materialColors.push("rgba(244, 67, 54,0.6)");
+materialColors.push("rgba(233, 30, 99,0.6)");
+materialColors.push("rgba(156, 39, 176,0.6)");
+materialColors.push("rgba(103, 58, 183,0.6)");
+materialColors.push("rgba(33, 150, 243,0.6)");
+materialColors.push("rgba(3, 169, 244,0.6)");
+materialColors.push("rgba(0, 188, 212,0.6)");
+materialColors.push("rgba(0, 150, 136,0.6)");
+materialColors.push("rgba(255, 235, 59,0.6)");
+materialColors.push("rgba(255, 87, 34,0.6)");
 document.addEventListener("DOMContentLoaded",function()
 {
 
@@ -29,50 +39,49 @@ function initCanvas()
 	}else
 	{
 		console.log("supported");
-		initLetters();
+		initParticles();
 		drawLetters();
 	}
 }
-function initLetters()
+function initParticles()
 {
-	var totalLetters=25;	
+	var totalParticles=25;	
 	var speed=[0.1,0.2,0.5,0.7];
-	var size=[3,4,5,6];
+	var size=[1,2,3,4];
 	var layers=4;
-	lettersList=[];	
+	particleList=[];	
 	for (var j = 0; j < layers; j++)
 	{
-		for (var i = 0; i < totalLetters; i++)
+		for (var i = 0; i < totalParticles; i++)
 		{
-			var letter={};
-			//⚫ MEDIUM BLACK CIRCLE 26AB ⬤ BLACK LARGE ...
-			letter.char="⚫";//String.fromCharCode(getRandomInt(97,122));
-			letter.isRequired=false;
-			letter.speed=speed[j];
-			letter.color="#555";
-			letter.x=getRandomInt(0,canvas.width);
-			letter.y=getRandomInt(0,canvas.height);
-			letter.size=size[j];
-			lettersList.push(letter);
+			var particle={};
+			particle.speed=speed[j];
+			particle.color=materialColors[getRandomInt(0,materialColors.length-1)];
+			particle.x=getRandomInt(0,canvas.width);
+			particle.y=getRandomInt(0,canvas.height);
+			particle.size=size[j];
+			particleList.push(particle);
 		}			
 	}
 }
 function drawLetters()
 {
 	context.clearRect(0,0,canvas.width,canvas.height);
-	for (var i = 0; i < lettersList.length; i++)
+	for (var i = 0; i < particleList.length; i++)
 	{
-		context.fillStyle=lettersList[i].color;
-		context.font=lettersList[i].size+"px sans-serif";
-		context.fillText(lettersList[i].char,lettersList[i].x,lettersList[i].y);
-		lettersList[i].x+=(lettersList[i].speed*textDir);
-		if(lettersList[i].x>canvas.width+10)
+		context.fillStyle=particleList[i].color;
+		context.beginPath();
+		context.arc(particleList[i].x,particleList[i].y,particleList[i].size,0,2*Math.PI);
+		context.closePath();
+		context.fill();
+		particleList[i].x+=(particleList[i].speed*textDir);
+		if(particleList[i].x>canvas.width+10)
 		{
-			lettersList[i].x=-10;
+			particleList[i].x=-10;
 		}
-		if(lettersList[i].x<-10)
+		if(particleList[i].x<-10)
 		{
-			lettersList[i].x=canvas.width+10;
+			particleList[i].x=canvas.width+10;
 		}
 	}	
 	requestAnimationFrame(drawLetters);
