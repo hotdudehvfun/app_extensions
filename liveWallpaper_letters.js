27c621e190ae7b442142d78d7de4b691581e01bc
hotdudehvfun <hotdudehvfun@gmail.com>
2017-08-02 12:53:57 +0530

    Add files via upload

--- liveWallpaper_letters.js
+++ liveWallpaper_letters.js
@@ -2,6 +2,17 @@ var canvas,context;
 var lettersList=[];
 var questionItem={};
 var textDir=1;
+var materialColors=[];
+materialColors.push("rgba(244, 67, 54,0.6)");
+materialColors.push("rgba(233, 30, 99,0.6)");
+materialColors.push("rgba(156, 39, 176,0.6)");
+materialColors.push("rgba(103, 58, 183,0.6)");
+materialColors.push("rgba(33, 150, 243,0.6)");
+materialColors.push("rgba(3, 169, 244,0.6)");
+materialColors.push("rgba(0, 188, 212,0.6)");
+materialColors.push("rgba(0, 150, 136,0.6)");
+materialColors.push("rgba(255, 235, 59,0.6)");
+materialColors.push("rgba(255, 87, 34,0.6)");
 document.addEventListener("DOMContentLoaded",function()
 {
 	setTimeout(function(){
@@ -45,7 +56,7 @@ function initLetters()
 			letter.char=String.fromCharCode(getRandomInt(97,122));
 			letter.isRequired=false;
 			letter.speed=speed[j];
-			letter.color="#111";
+			letter.color=materialColors[getRandomInt(0,materialColors.length-1)];
 			letter.x=getRandomInt(0,canvas.width);
 			letter.y=getRandomInt(0,canvas.height);
 			letter.size=size[j];
